030ed492dd8b37f1547bcd803a6bfc082d67cadb
skn <simon.knoblich@student.reutlingen-university.de>
2023-11-08 09:29:13 +0100

    Ausgabe von Spiel angepasst

--- basic_backend/PrintBoard.java
+++ basic_backend/PrintBoard.java
@@ -2,8 +2,9 @@ package basic_backend;
 
 public class PrintBoard {
 
-    public static void printBoard(int field[][]) {
+    private static final int PRINTLINE = 57;
 
+    public static void printBoard(int field[][]) {
 
         //Kontrolle um das print feld aus richtig ist
 /*
@@ -13,11 +14,12 @@ public class PrintBoard {
             }
         }
 */
-        for(int i=0; i<50; i++)
+        for(int i=0; i<PRINTLINE; i++)
             System.out.printf("-");
         System.out.println();
         // Gibt das Spielfeld auf der Konsole aus
         for(int i = 0; i < 5; i++) {
+            System.out.print("|\t");
             for (int j = 5; j >= 0; j--) {
                 System.out.print(field[j+6][i] + "\t");
             }
@@ -25,13 +27,15 @@ public class PrintBoard {
             for (int j2 = 5; j2 >= 0; j2--) {
                 System.out.print(field[j2][i] + "\t");
             }
+            System.out.print("|\t");
             System.out.println();
         }
-        for(int i=0; i<50; i++)
+        for(int i=0; i<PRINTLINE; i++)
             System.out.printf("-"); // Trennlinie die beiden Spielhälften in zwei Teile teilt
 
         System.out.println();
         for (int a = 4; a >= 0; a--) {
+            System.out.print("|\t");
             for (int k = 0; k < 6; k++) {
                 System.out.print(field[k + 12][a] + "\t");
             }
@@ -39,9 +43,10 @@ public class PrintBoard {
             for (int k2 = 0; k2 < 6; k2++) {
                 System.out.print(field[k2 + 18][a] + "\t");
             }
+            System.out.print("|\t"); // Teilt die Spielseite in einen 6er Block
             System.out.println();
         }
-        for(int i=0; i<50; i++)
+        for(int i=0; i<PRINTLINE; i++)
             System.out.printf("-");
         System.out.println();
     }
