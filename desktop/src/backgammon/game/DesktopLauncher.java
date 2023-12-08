584e0f7ae37a27c318f2574e2bfee64175bf8314
skn <simon.knoblich@student.reutlingen-university.de>
2023-12-08 12:38:03 +0100

    Klassen hinzugefügt

--- desktop/src/backgammon/game/DesktopLauncher.java
+++ desktop/src/backgammon/game/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package backgammon.game;
 
+import backgammon.game.basic_backend.*;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import backgammon.game.Backgammon;
@@ -7,6 +8,7 @@ import backgammon.game.Backgammon;
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+		Rules r = new Rules();
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Backgammon");
