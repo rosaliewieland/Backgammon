a8615b748ad7a82778816513e2d55d118373044e
woodapples <marco.holzeapfel@protonmail.com>
2023-12-12 14:15:36 +0100

    Verbesserungen am Brett vorgenommen, Icons für die Player erstellt

---
+++ core/src/backgammon/game/screens/PlayerSettings.java
@@ -0,0 +1,26 @@
+package backgammon.game.screens;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+public class PlayerSettings extends ScreenAdapter {
+
+    public PlayerSettings() {
+
+
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+
+    @Override
+    public void hide() {
+        this.dispose();
+    }
+}
