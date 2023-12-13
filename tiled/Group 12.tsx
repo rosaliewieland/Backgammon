fdead6c424b68289c62b13dcc6a18a250dd19241
woodapples <marco.holzeapfel@protonmail.com>
2023-12-13 15:50:49 +0100

    Verbesserungen am Brett vorgenommen, Icons für die Player erstellt

---
+++ tiled/Group 12.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Group 12" tilewidth="64" tileheight="64" tilecount="56" columns="8">
+ <image source="Group 12.png" width="512" height="448"/>
+</tileset>
