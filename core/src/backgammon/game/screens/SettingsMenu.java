b0ed5c59dd0d983f3676d408c998866cc6c5638c
CTGame1 <ceyhun.taskin@student.reutlingen-university.de>
2023-12-13 11:13:53 +0100

    Menu Progress SettingsMenu hinzugefügt fortschritt mit ScenceSwitch

--- core/src/backgammon/game/screens/SettingsMenu.java
+++ core/src/backgammon/game/screens/SettingsMenu.java
@@ -6,14 +6,11 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import java.io.ObjectInputFilter;
 
-public class MainMenu implements Screen{
+public class SettingsMenu implements Screen{
     //nehme Backgammon Klasse
     Backgammon game;
     //Hinzufüge Texturen
@@ -23,13 +20,14 @@ public class MainMenu implements Screen{
     private Texture quitbutton;
     private OrthographicCamera menucam;
     private Viewport menuport;
-    public MainMenu(Backgammon game) {
+    public SettingsMenu(Backgammon game) {
         this.game = game;
         background = new Texture("MainMenu.png");
         startbutton = new Texture("PlayButton.png");
         settingsbutton = new Texture("SettingsButton.png");
         quitbutton = new Texture("QuitButton.png");
-
+        menucam = new OrthographicCamera();
+        menuport = new ScreenViewport(menucam);
     }
 
     @Override
@@ -42,18 +40,23 @@ public class MainMenu implements Screen{
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.setProjectionMatrix(menucam.combined);
         game.batch.begin();
-        game.batch.draw(background, 0, 0);
+        game.batch.draw(background, -600, -475);
         game.batch.draw(startbutton, -150, 45);
         game.batch.draw(settingsbutton, -325, -100);
         game.batch.draw(quitbutton, 25, -100);
         game.batch.end();
-
+        if(Gdx.input.isTouched()){
+            Vector3 touchPos = new Vector3();
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+            menucam.unproject(touchPos);
+        }
     }
 
     @Override
     public void resize(int width, int height) {
         menuport.update(width,height);
         menucam.update();
+
     }
 
     @Override
