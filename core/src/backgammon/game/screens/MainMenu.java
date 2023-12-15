2b709b49f8503563dac3e441dd3764263bd4eb18
woodapples <marco.holzeapfel@protonmail.com>
2023-12-15 18:04:40 +0100

    Aktueller Stand Marco

--- core/src/backgammon/game/screens/MainMenu.java
+++ core/src/backgammon/game/screens/MainMenu.java
@@ -2,75 +2,96 @@ package backgammon.game.screens;
 
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
-public class MainMenu extends ScreenAdapter implements InputProcessor {
+public class MainMenu extends ScreenAdapter {
 
-    public SpriteBatch batch;
-    private ImageButton quitButton;
-    private ImageButton startButton;
-    private ImageButton settingsButton;
+    private Stage stage;
+    private Viewport viewport;
 
-
-    private final Texture background;
-    private final Texture startbutton = new Texture("PlayButton.png");
-    private final Texture settingsbutton = new Texture("SettingsButton.png");
-    private final Texture quitbutton = new Texture("QuitButton.png");
-    private final OrthographicCamera menucam;
-    private final Viewport menuport;
+    private Texture myTexture;
+    private TextureRegion myTextureRegion;
+    private TextureRegionDrawable myTexRegionDrawable;
+    private ImageButton quiteTheGame;
+    private Table mainTable;
+    private Batch batch;
+    private Texture background;
 
 
     public MainMenu() {
         batch = new SpriteBatch();
         background = new Texture("MainMenu.png");
-        menucam = new OrthographicCamera();
-        menuport = new ScreenViewport(menucam);
-        windowQuitButton();
-        windowSettingsButton();
-        windowStartButton();
-        Gdx.input.setInputProcessor(this);
+
+    }
+
+
+    @Override
+    public void show() {
+        viewport = new ExtendViewport(1200, 950);
+        stage = new Stage(viewport);
+        mainTable = new Table();
+        mainTable.setFillParent(true);
+        stage.addActor(mainTable);
+
+        addButton("PlayButton.png").addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("Nächster Bildschirm");
+            }
+        });
+        addButton("SettingsButton.png").addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("Settings wurden geöffnet!");
+            }
+        });
+        addButton("QuitButton.png").addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
+        Gdx.input.setInputProcessor(stage);
+
     }
 
     @Override
     public void render(float delta){
+
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.setProjectionMatrix(menucam.combined);
+
         batch.begin();
-        batch.draw(background, -600, -475);
-        quitButton.draw(batch, 1);
-        startButton.draw(batch,1);
-        settingsButton.draw(batch,1);
+        batch.draw(background, 0, 0);
         batch.end();
+
+        stage.act();
+        stage.draw();
 
     }
 
     @Override
     public void resize(int width, int height) {
-        menuport.update(width,height);
-        menucam.update();
+        viewport.update(width, height);
+
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        background.dispose();
-        startbutton.dispose();
-        settingsbutton.dispose();
-        quitbutton.dispose();
     }
 
     @Override
@@ -78,95 +99,13 @@ public class MainMenu extends ScreenAdapter implements InputProcessor {
         this.dispose();
     }
 
+    private ImageButton addButton(String name) {
+        myTexture = new Texture(Gdx.files.internal(name));
+        myTextureRegion = new TextureRegion(myTexture);
+        myTexRegionDrawable = new TextureRegionDrawable(myTextureRegion);
+        quiteTheGame = new ImageButton(myTexRegionDrawable);
+        mainTable.add(quiteTheGame).padBottom(20).padRight(20);
 
-    public void windowQuitButton(){
-        TextureRegionDrawable quitButtonDrawable = new TextureRegionDrawable(new TextureRegion(quitbutton));
-        quitButton = new ImageButton(quitButtonDrawable);
-        quitButton.setPosition(25, -100);
-
-        quitButton.addListener(new ClickListener() {
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-
-                return true;
-            }
-        });
-
-
-    }    public void windowStartButton(){
-        TextureRegionDrawable startButtonDrawable = new TextureRegionDrawable(new TextureRegion(startbutton));
-        startButton = new ImageButton(startButtonDrawable);
-        startButton.setPosition(-150, 45);
-
-        startButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                Gdx.app.exit();
-            }
-        });
-
-
-    }    public void windowSettingsButton(){
-        TextureRegionDrawable settingButtonDrawable = new TextureRegionDrawable(new TextureRegion(settingsbutton));
-        settingsButton = new ImageButton(settingButtonDrawable);
-        settingsButton.setPosition(-325, -100);
-
-        settingsButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                Gdx.app.log("ClickListener", "Einstellungsbutton wurde geklickt. Koordinaten: (" + x + ", " + y + ")");
-
-                Gdx.app.exit();
-            }
-        });
-    }
-
-    @Override
-    public boolean keyDown(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyUp(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyTyped(char character) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        if(button == Input.Buttons.LEFT) {
-            Gdx.app.log("ClickListener", "TouchDown auf dem Einstellungsbutton");
-        }
-        return false;
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(float amountX, float amountY) {
-        return false;
+        return quiteTheGame;
     }
 }
-
