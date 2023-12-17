ffb49d7224bf3836ca2226034380b13374746ee9
skn <simon.knoblich@student.reutlingen-university.de>
2023-12-17 15:37:38 +0100

    Sync mit Rosie

--- core/src/backgammon/game/Backgammon.java
+++ core/src/backgammon/game/Backgammon.java
@@ -1035,6 +1035,7 @@ public class Backgammon extends ScreenAdapter implements InputProcessor{
 			if(isMovedDiceOne && isMovedDiceTwo)
 			{
 				whoesTurn();
+
 			}
 			if(playerOne.isTheWinner())
 			{
